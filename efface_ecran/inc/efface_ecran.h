1b3e719d6f097d5b6f422a7aa95b1771002091fe
erba <eric.bachard@free.fr>
2018-08-02 22:45:45 +0200

    Switching to MIT license for this piece of code

--- efface_ecran/inc/efface_ecran.h
+++ efface_ecran/inc/efface_ecran.h
@@ -2,8 +2,8 @@
 /* efface_ecran.h fichier d'en tete */
 /*
  * Auteur:  Eric Bachard  / 3 novembre 2004
- * Ce document est sous Licence GPL v2
- * voir : http://www.gnu.org/licenses/gpl-2.0.html
+ * Ce document est sous Licence MIT
+ * voir LICENCE.txt dans le répertoire parent
  */
 
 #ifndef __EFFACE_ECRAN_H
