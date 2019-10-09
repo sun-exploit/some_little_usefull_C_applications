5f722cd79b52ebe928f3733622b1ff93c72aab90
erba <eric.bachard@free.fr>
2019-10-09 11:50:01 +0200

    Use better name for random intialization

---
+++ Documents/Partie1/TD/test_memoire_utilisateur/inc/initialize_rand.h
@@ -0,0 +1,13 @@
+/* initialize_rand.h fichier d'en tete */
+/*
+ * Auteur:  Eric Bachard  / 3 novembre 2014
+ * Ce document est sous Licence GPL v2
+ * voir : http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+#ifndef __INITIALIZE_RAND_H
+#define __INITIALIZE_RAND_H
+
+void initialize_rand(void);
+
+#endif /* __INITIALIZE_RAND_H */
