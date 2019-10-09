24bbeb92ba845cb47a33d88e091c1b38ebd9e12b
erba <eric.bachard@free.fr>
2019-10-09 12:03:03 +0200

    Cosmetics + improve i/o

--- Documents/Partie1/TD/test_memoire_utilisateur/sources/test_memoire.c
+++ Documents/Partie1/TD/test_memoire_utilisateur/sources/test_memoire.c
@@ -21,6 +21,7 @@ short initialisation(int *tableau_initial)
     {
         tableau_initial[i] = 0;
     }
+
     return EXIT_SUCCESS;
 }
 /* --------------------------------- */
@@ -34,7 +35,9 @@ short tirage_tableau_initial(int *tableau_initial)
     int indice;
     int alea = 0;
     bool sortie;
+
     initialize_rand();
+
     for ( indice = 0; indice < ARRAY_SIZE ; indice++) 
     {
         do {
@@ -71,9 +74,12 @@ short generer_numero_element_tableau(int tableau_initial[])
 {
     initialize_rand();
     short alea2;
-    do {
+
+    do
+    {
         /* generate a value between 0 and 9 */
         alea2 = rand()%ARRAY_SIZE;
+
     } while(0 == tableau_initial[alea2]);
 
     return alea2;
@@ -88,18 +94,21 @@ short generer_numero_element_tableau(int tableau_initial[])
 short afficher_question(int value)
 {
     char *suffixe;
+
     switch (value)
     {
-    
-    /* the first array element has index 0*/
-    case (0):
-        suffixe = "er";
-        break;
-    default:
-        suffixe = "eme";
-        break;
+
+        /* the first array element has index 0*/
+        case (0):
+            suffixe = "er";
+            break;
+        default:
+            suffixe = "eme";
+            break;
     }
+
     fprintf( stdout, "Donner le %d%s element du tableau \n\n",value + 1, suffixe);
+
     return EXIT_SUCCESS;
 }
 
@@ -110,9 +119,12 @@ short afficher_question(int value)
 short lire_reponse(void)
 {
     short number;
+
     fprintf(stdout, "Entrer votre reponse et appuyer sur entree \n");
+
     number = saisie_nombre_entier_court(VAL_MIN, VAL_MAX);
     efface_ecran();
+
     return number;
 }
 
@@ -127,17 +139,18 @@ short affichage_resultat_question(int tableau_initial[], int rang, int number, i
     /*hack*/
     fprintf(stdout, "vrai = %d ",tableau_initial[rang]);
     fprintf(stdout,"entre = %d \n",number); 
+
     if( number == tableau_initial[rang] )
     {
         fprintf(stdout, "C'est juste \n");
         *p_nombre_de_points += 2;
-	/*hack*/
-	printf("Nombre de points local %d \n", *p_nombre_de_points);
+        /*hack*/
+        fprintf(stdout, "Nombre de points local %d \n", *p_nombre_de_points);
     }
     else
         fprintf(stdout,"C'est faux \n");
 
-/*	getchar();
+    /*getchar();
     efface_ecran();*/
     return EXIT_SUCCESS;
 }
@@ -161,7 +174,7 @@ void effacer_element_tableau(int rang, int tableau_initial[])
 
 void affichage_resultat_final(int points)
 {
-        fprintf(stdout, "Nombre de points final =  %d \n", points); 
+    fprintf(stdout, "Nombre de points final =  %d \n", points); 
 }
 
 /* --------------------------------- */
@@ -173,6 +186,7 @@ void affichage_tableau_initial(int tableau_initial[])
 {
     int i;
     for( i = 0 ; i < ARRAY_SIZE; i++)
+
     fprintf(stdout,"Tirage %2d : %d   \n",i + 1, tableau_initial[i]);
     fprintf(stdout,"\n");
 }
@@ -184,16 +198,20 @@ void affichage_tableau_initial(int tableau_initial[])
 /* ---------- main ----------- */
 
 int main(void)
-{        
+{
     int nombre_de_points = 0;
     int *p_nombre_de_points = &nombre_de_points; 
     int tableau_initial[ARRAY_SIZE];
+
     initialisation(tableau_initial);
     tirage_tableau_initial(tableau_initial);
     affichage_tableau_initial(tableau_initial);
+
     fprintf(stdout," appuyer sur Entree pour continuer");
     getchar();
+
     efface_ecran();
+
     int k;
     for(k = 0; k < ARRAY_SIZE; k++)
     {
@@ -202,10 +220,10 @@ int main(void)
         short reponse = lire_reponse();
         affichage_resultat_question(tableau_initial, rang, reponse, p_nombre_de_points);
         effacer_element_tableau(rang,tableau_initial);
-    
     }
+
     affichage_resultat_final(nombre_de_points);
-  	return EXIT_SUCCESS; 
+    return EXIT_SUCCESS; 
 }
 
 /* --------------------------------- */
