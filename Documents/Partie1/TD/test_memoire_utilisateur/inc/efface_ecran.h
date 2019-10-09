5cb5d0759ee1d9aeb336fd7f1b5e6721720b1393
erba <eric.bachard@free.fr>
2019-10-09 11:40:54 +0200

    Add efface_ecran.h. This file is under MIT license too

---
+++ Documents/Partie1/TD/test_memoire_utilisateur/inc/efface_ecran.h
@@ -0,0 +1,14 @@
+
+/* efface_ecran.h fichier d'en tete */
+/*
+ * Auteur:  Eric Bachard  / 3 novembre 2004
+ * Ce document est sous Licence MIT
+ * voir LICENCE.txt dans le répertoire parent
+ */
+
+#ifndef __EFFACE_ECRAN_H
+#define __EFFACE_ECRAN_H
+
+void efface_ecran(void);
+
+#endif /* __EFFACE_ECRAN_H */
