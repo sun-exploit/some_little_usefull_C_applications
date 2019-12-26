9f5bb43326aa05e190d13516aca12118610eb21a
erba <eric.bachard@free.fr>
2019-12-26 22:21:15 +0100

    Ajout de hash_letter(), dehash_letter() pour obtenir un hash en temps réel

--- Jules_Cesar_personnalise/inc/strings_utils.h
+++ Jules_Cesar_personnalise/inc/strings_utils.h
@@ -7,6 +7,18 @@
 #ifndef __STRING_UTILS__H__
 #define __STRING_UTILS__H__
 
+int clear_string(char *);
+
+void hash_letter(char, int *, int *);
+
+void dehash_letter(char, int *, int *);
+
+int hash_func(char *, int *);
+
+int check_size(char *);
+
+void new_word(char *, int *);
+
 int new_string(char *, char *, int *, int *);
 
 int refresh_screen(char *, char *, int *, int *);
