cd64fc0c064053d6ca9fc022a2d4f1bd9b2f8fa3
erba <eric.bachard@free.fr>
2020-07-18 13:42:11 +0200

    Amélioration de la présentation, et mise en place de la version pleine de warnings

--- fonction_de_hashage/sources/hashage.c
+++ fonction_de_hashage/sources/hashage.c
@@ -1,4 +1,4 @@
-/* Copyright  Eric Bachard  / 17 novembre 2014
+/* Auteur:  Eric Bachard  / 17 novembre 2014
  * Ce document est sous Licence GPL v2
  * voir : http://www.gnu.org/licenses/gpl-2.0.html
  */
@@ -28,11 +28,11 @@ extern HASH * my_hash_table;
 HASH *initHash (void)
 {
 #ifdef DEBUG
-    fprintf(stderr, "Fonction en cours d'exécution : %s \n", __func__ );
+  fprintf(stderr, "Fonction en cours d'exécution : %s \n", __func__ );
 #endif
 
     // Declaration d'un pointeur vers une structure de type HASH
-    // ATTENTION : le pointeur est initialisé a NULL dans main, pasici !
+    // ATTENTION : le pointeur est initialisé a NULL dans main, pas ici !
 
     /* malloc => heap allocation */
     my_hash_table = (HASH *)malloc(sizeof(HASH));
@@ -55,22 +55,22 @@ HASH *initHash (void)
 
 
 
-void destroyHash (HASH *p_hash_table)
+void destroyHash (HASH * my_hash_table)
 {
 #ifdef DEBUG
     fprintf(stderr, "Fonction en cours d'exécution : %s \n", __func__ );
 #endif
     HASH * my_hash_table_next;
 
-    my_hash_table_next = p_hash_table->next;
+    my_hash_table_next = my_hash_table->next;
 
     /* delete the element  H is pointing to */
-    if ((p_hash_table != NULL ) && (refcount > 0))
+    if ((my_hash_table != NULL ) && (refcount > 0))
     {
-        free(p_hash_table);
-        refcount--;
+      free(my_hash_table);
+      refcount--;
 #ifdef DEBUG
-        fprintf(stderr, "Décrémentation de refcount. Nouvelle valeur =  %d \n", refcount );
+      fprintf(stderr, "Décrémentation de refcount. Nouvelle valeur =  %d \n", refcount );
 #endif
     }
 
@@ -81,12 +81,12 @@ void destroyHash (HASH *p_hash_table)
 }
 
 
-void setHashValue (HASH * p_hash_table, char key[20], int value)
+void setHashValue (HASH * my_hash_table, char key[20], int value)
 {
 #ifdef DEBUG
     fprintf(stderr, "Fonction en cours d'exécution : %s \n", __func__ );
 #endif
-    unsigned int i;
+    int i;
 
     /*
         Tant que l'on est pas arrive au bout de la table de hashage
@@ -95,60 +95,60 @@ void setHashValue (HASH * p_hash_table, char key[20], int value)
     */
 
 
-    while((p_hash_table->next != NULL) && (strcmp (p_hash_table->key , key) != 0))
+    while((my_hash_table->next != NULL) && (strcmp (my_hash_table->key , key) != 0))
     {
-        p_hash_table = p_hash_table->next;
+        my_hash_table = my_hash_table->next;
     }
 
     /* attention piège : strcmp compare 2 chaines de caractères et renvoie 0 ssi les 2 chaines sont identiques */
 
-    if( strcmp (p_hash_table->key, key) == 0 )
-        p_hash_table->value = value;
+    if( strcmp (my_hash_table->key, key) == 0 )
+        my_hash_table->value = value;
     else
     {
-        p_hash_table->next = (HASH *)malloc(sizeof(HASH));
+        my_hash_table->next = (HASH *)malloc(sizeof(HASH));
 
-        if(p_hash_table->next == NULL)
+        if(my_hash_table->next == NULL)
             exit(EXIT_FAILURE);
 
-        p_hash_table = p_hash_table->next;
+        my_hash_table = my_hash_table->next;
 
         for ( i=0; i<=strlen(key); i=i+1 )
         {
-            p_hash_table->key[i]=key[i];
+            my_hash_table->key[i]=key[i];
         }
 
-        p_hash_table->value = value;
-        p_hash_table->next = NULL;
+        my_hash_table->value = value;
+        my_hash_table->next = NULL;
     }
 }
 
 
-int getHashValue ( HASH * p_hash_table, char key[20] )
+int getHashValue (HASH * my_hash_table, char key[20])
 {
 
 #ifdef DEBUG
     fprintf(stderr, "Fonction en cours d'exécution : %s \n", __func__ );
 #endif
 
-    while(strcmp( p_hash_table->key , key) != 0)
+    while(strcmp( my_hash_table->key , key) != 0)
     {
-        p_hash_table = p_hash_table->next;
+        my_hash_table = my_hash_table->next;
     }
 
-    return p_hash_table->value;
+    return my_hash_table->value;
 }
 
-bool b_IsHashKey (HASH * p_hash_table, char key[20] )
+bool b_IsHashKey (HASH * my_hash_table, char key[20])
 {
     bool toReturn = false;
 
-    while ( (p_hash_table != NULL) && ( strcmp ( p_hash_table->key , key ) != 0 ) )
+    while ( (my_hash_table != NULL) && ( strcmp ( my_hash_table->key , key ) != 0 ) )
     {
-        p_hash_table = p_hash_table->next;
+        my_hash_table = my_hash_table->next;
     }
 
-    if ( (p_hash_table != NULL) && (strcmp(p_hash_table->key , key) == 0 ) )
+    if ( (my_hash_table != NULL) && (strcmp(my_hash_table->key , key) == 0 ) )
         return true;
     else
       toReturn =false;
@@ -156,25 +156,28 @@ bool b_IsHashKey (HASH * p_hash_table, char key[20] )
     return toReturn;
 }
 
-void deleteHashKey (HASH * p_hash_table, char key[20] )
+void deleteHashKey (HASH * my_hash_table, char key[20] )
 {
 #ifdef DEBUG
     fprintf(stderr, "Fonction en cours d'exécution : %s \n", __func__ );
 #endif
     HASH *hash_to_delete;
 
-    while (strcmp ( p_hash_table->next->key , key ) != 0  )
+    while (strcmp ( my_hash_table->next->key , key ) != 0  )
     {
-        p_hash_table = p_hash_table->next;
+        my_hash_table = my_hash_table->next;
     }
 
-    /*Le pointeur hash_to_delete va pointer vers l'element a supprimer.
-        Il ne faut pas oublier de lier l'element precedent de
-        N avec l'element suivant de N pour ne pas casser la liste chainée. */
-    hash_to_delete = p_hash_table->next;
+    /*
+        Le pointeur hash_to_delete va pointer vers l'element a supprimer.
+        Il ne faut pas oublier de lier l'element precedent de N avec l'element suivant de N 
+        pour ne pas casser la liste chainée.
+    */
 
-    if (p_hash_table->next->next != NULL)
-        p_hash_table->next = p_hash_table->next->next;
+    hash_to_delete = my_hash_table->next;
+
+    if (my_hash_table->next->next != NULL)
+        my_hash_table->next = my_hash_table->next->next;
 
     if (( hash_to_delete != NULL ) && ( refcount > 0 ))
         free(hash_to_delete);
