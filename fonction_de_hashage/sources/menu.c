fc4be298a2e380f21120bc7f2c8026c26638bed0
erba <eric.bachard@free.fr>
2020-07-18 17:10:12 +0200

    Code cleanup

--- fonction_de_hashage/sources/menu.c
+++ fonction_de_hashage/sources/menu.c
@@ -13,123 +13,127 @@
 void menu(bool b_Quit)
 {
 #ifdef DEBUG
-  fprintf(stderr, "Fonction en cours d'exécution : %s \n", __func__ );
+    fprintf(stderr, "Fonction en cours d'exécution : %s \n", __func__ );
 #endif
-  char choice = 'a';
-  char key[20];
-  int val;
-  extern HASH * my_hash_table;// = NULL;
+    char choice = 'a';
+    char key[20];
+    int val;
+    extern HASH * my_hash_table;// = NULL;
 
-  while (false == b_Quit)
-  {
-    display();
-
-    // TODO : use saisie_nombre_entier_court()
-    scanf ("%c", &choice);
-
-    while (getchar() != '\n');
-
-    switch (choice)
+    while (false == b_Quit)
     {
-      case HASH_AFFECT_VALUE_FOR_GIVEN_KEY:
-      {
-        fprintf(stdout, "Entrez la clé de l'élément à modifier/crée\n");
+        display();
 
-        // TODO : utiliser saisie_chaine_de_caracteres
-        scanf("%s",key);
+        // TODO : use saisie_nombre_entier_court()
+        scanf ("%c", &choice);
 
         while (getchar() != '\n');
+
+        switch (choice)
+        {
+            case HASH_AFFECT_VALUE_FOR_GIVEN_KEY:
+            {
+                fprintf(stdout, "Entrez la clé de l'élément à modifier/crée\n");
+
+                // TODO : utiliser saisie_chaine_de_caracteres
+                scanf("%s",key);
+
+                while (getchar() != '\n');
 #ifdef DEBUG
-        fprintf(stderr, "key vaut : %s \n", key );
+                fprintf(stderr, "key vaut : %s \n", key );
 #endif
-        fprintf(stdout, "Entrez la valeur de l'élément\n");
+                fprintf(stdout, "Entrez la valeur de l'élément\n");
 
 
-        // TODO : utiliser saisie_nombre_reel()
-        scanf("%d",&val);
-        while (getchar() != '\n');
+                // TODO : utiliser saisie_nombre_reel()
+                scanf("%d",&val);
+                while (getchar() != '\n');
 
-        push( my_hash_table, key, val);
+                push( my_hash_table, key, val);
 
-        break;
-      }
+                break;
+            }
 
-      case HASH_RETURN_VALUE_FOR_GIVEN_KEY:
-      {
-        fprintf(stdout, "Entrez la clé de l'élément\n");
+            case HASH_RETURN_VALUE_FOR_GIVEN_KEY:
+            {
+                fprintf(stdout, "Entrez la clé de l'élément\n");
 
-        // TODO : utiliser saisie_chaine_de_caracteres
-        scanf("%s",key);
-        while (getchar() != '\n');
+                // TODO : utiliser saisie_chaine_de_caracteres
+                scanf("%s",key);
+                while (getchar() != '\n');
 
-        if( true == b_IsHashKey(my_hash_table,key))
-          fprintf(stdout, "La valeur de l'élément est %d",getHashValue(my_hash_table,key));
-        else
-          fprintf(stdout, "Aucun élément de la table ne contient cette clé\n");
+                if( true == b_IsHashKey(my_hash_table,key))
+                    fprintf(stdout, "La valeur de l'élément est %d",getHashValue(my_hash_table,key));
+                else
+                    fprintf(stdout, "Aucun élément de la table ne contient cette clé\n");
 
-        break;
-      }
+            break;
+            }
 
-      case HASH_SEARCH_A_KEY:
-      {
-        fprintf(stdout, "Entrez la clé de l'élément\n");
+            case HASH_SEARCH_A_KEY:
+            {
+                fprintf(stdout, "Entrez la clé de l'élément\n");
 
-        // TODO : utiliser saisie_chaine_de_caracteres
-        scanf("%s",key);
+                // TODO : utiliser saisie_chaine_de_caracteres
+                scanf("%s",key);
 #ifdef DEBUG
-        fprintf(stderr, "key vaut : %s \n", key );
+                fprintf(stderr, "key vaut : %s \n", key );
 #endif
-        while (getchar() != '\n');
+                while (getchar() != '\n');
 
-        if (b_IsHashKey(my_hash_table,key) == 1)
-          fprintf(stdout, "L'élément existe\n");
-        else
-          fprintf(stdout, "L'élément n'existe pas\n");
+                if (b_IsHashKey(my_hash_table,key) == 1)
+                    fprintf(stdout, "L'élément existe\n");
+                else
+                    fprintf(stdout, "L'élément n'existe pas\n");
 
-        break;
-      }
+            break;
+            }
 
-      case HASH_DELETE_ENTRY_FROM_GIVEN_KEY:
-      {
-        fprintf(stdout, "Entrez la clé de l'élément\n");
+            case HASH_DELETE_ENTRY_FROM_GIVEN_KEY:
+            {
+                fprintf(stdout, "Entrez la clé de l'élément\n");
 
-        // TODO : utiliser saisie_chaine_de_caracteres
-        scanf("%s",key);
-        while (getchar() != '\n');
+                // TODO : utiliser saisie_chaine_de_caracteres
+                scanf("%s",key);
+                while (getchar() != '\n');
 
 #ifdef DEBUG
-        fprintf(stderr, "key vaut : %s \n", key );
+                fprintf(stderr, "key vaut : %s \n", key );
 #endif
-        if (false == b_IsHashKey(my_hash_table,key))
-          fprintf(stdout, "L'élément n'existe pas!\n");
-        else
-          pop(my_hash_table,key);
+                if (false == b_IsHashKey(my_hash_table,key))
+                    fprintf(stdout, "L'élément n'existe pas!\n");
+                else
+                    pop(my_hash_table,key);
 
-        break;
-      }
+            }
+            break;
 
-      case HASH_DELETE_TABLE:
-      {
-        if (my_hash_table != NULL)
-            eraseTable(my_hash_table);
+            case HASH_DELETE_TABLE:
+            {
+                if (my_hash_table != NULL)
+                    eraseTable(my_hash_table);
 
-        break;
-      }
-      case HASH_TABLE_QUIT:
-      {
+            }
+            break;
+
+            case HASH_TABLE_QUIT:
+            {
 #ifdef DEBUG
-        fprintf(stderr, "Sortie du programme. key vaut : %s \n", key );
+                fprintf(stderr, "Sortie du programme. key vaut : %s \n", key );
 #endif
-        b_Quit = true;
+                b_Quit = true;
 
-        if(my_hash_table != NULL)
-          eraseTable(my_hash_table);
+                if(my_hash_table != NULL)
+                    eraseTable(my_hash_table);
 
-        break;
-      }
-        default:
-          fprintf(stdout, "Mauvaise saisie\n");
+            }
+            break;
+
+            default:
+                fprintf(stdout, "Mauvaise saisie\n");
+
+            break;
+        }
     }
-  }
 }
 
